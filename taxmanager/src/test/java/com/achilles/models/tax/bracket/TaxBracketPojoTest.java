7ee32cc3db8e7f3187d3568c0c80876b118fd8d8
Allan Chua <allanchua.officefiles@gmail.com>
2019-02-23 22:53:54 +0800

    -Created unit test for tax bracket accessors

---
+++ taxmanager/src/test/java/com/achilles/models/tax/bracket/TaxBracketPojoTest.java
@@ -0,0 +1,51 @@
+package com.achilles.models.tax.bracket;
+
+import com.achilles.models.TaxBracket;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TaxBracketPojoTest {
+    //#region Private Methods
+    private TaxBracket buildFakeBracket() {
+        double fakeMin = 0;
+        double fakeMax = 0;
+        double fakeRate = 0;
+
+        return new TaxBracket(fakeMin, fakeMax, fakeRate);
+    }
+    //#endregion
+
+    @Test
+    public void testMinAmountAccessors() {
+        double expected = 50000000;
+        TaxBracket bracket = buildFakeBracket();
+
+        bracket.setMinAmount(expected);
+        double output = bracket.getMinAmount();
+
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    public void testCapAmountAccessors() {
+        double expected = 50000000;
+        TaxBracket bracket = buildFakeBracket();
+
+        bracket.setCapAmount(expected);
+        double output = bracket.getCapAmount();
+
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    public void testRateAccessors() {
+        double expected = 50000000;
+        TaxBracket bracket = buildFakeBracket();
+
+        bracket.setTaxRate(expected);
+        double output = bracket.getTaxRate();
+
+        assertThat(output).isEqualTo(expected);
+    }
+}
