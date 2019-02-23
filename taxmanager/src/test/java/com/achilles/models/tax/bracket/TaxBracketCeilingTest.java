234e22dbae514464319884d8c0a6a7e9ba998d89
Allan Chua <allanchua.officefiles@gmail.com>
2019-02-23 18:55:14 +0800

    -Introduced tax ceiling unit testing

---
+++ taxmanager/src/test/java/com/achilles/models/tax/bracket/TaxBracketCeilingTest.java
@@ -0,0 +1,47 @@
+package com.achilles.models.tax.bracket;
+
+import com.achilles.models.TaxBracket;
+import com.achilles.utilities.AnnualTaxableIncomeCalculator;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(Parameterized.class)
+public class TaxBracketCeilingTest {
+    //#region Fields
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                { -1, true },
+                { 10000, false },
+                { 100000, false }
+        });
+    }
+
+    private double input;
+    private boolean expected;
+    //#endregion
+
+    //#region Constructors
+    public TaxBracketCeilingTest(double input, boolean expected) {
+        this.input = input;
+        this.expected = expected;
+    }
+    //#endregion
+
+    //#region Public Methods
+    @Test
+    public void testTaxBracketCeilings() {
+        double fakeMin = 0;
+        double fakeRate = 0;
+        TaxBracket bracket = new TaxBracket(fakeMin, input, fakeRate);
+
+        assertThat(bracket.isCeiling()).isEqualTo(expected);
+    }
+    //#endregion
+}
